3419e421f9bcc28988ff5a17f8d3b18a6ee7758a
haijun12 <79031887+haijun12@users.noreply.github.com>
2021-12-12 13:56:29 -0600

    Update products.js

--- products.js
+++ products.js
@@ -201,7 +201,7 @@ app.post('/add', (req, res) => {
     var dbo = db.db('reveauchocolat');
 	var products = dbo.collection('products');
 	var currUser = dbo.collection('current');
-	
+	var user = dbo.collection('users');
 		let quan = [];
 		quan.push(req.body.quan0);
 		quan.push(req.body.quan1);
@@ -230,7 +230,7 @@ app.post('/add', (req, res) => {
 			quan.push(req.body.quan22);
 			quan.push(req.body.quan23);
 		}
-		var user = dbo.collection('users');
+		
 		currUser.findOne({current: "current"}, function (err, currUser) {
 			if (err) throw err;
 			var query = {
@@ -269,6 +269,7 @@ app.post('/add', (req, res) => {
 								}
 							}
 						)
+						res.write("You added to Cart");
 					})
 
 				}
@@ -276,67 +277,6 @@ app.post('/add', (req, res) => {
 		}); // end finding curr user
     })
     
-    fs.readFile('cart.html', function (err, txt) {
-        if (err) throw (err);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-
-        res.write(txt);
-        MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-            if(err) { return console.log(err); }
-
-            var dbo = db.db('reveauchocolat');
-            var user = dbo.collection('users');
-            var currUser = dbo.collection('current');
-			currUser.findOne({current: "current"}, function (err, currUser) {
-			var query = {
-				email : currUser.email
-			}
-		    user.findOne(query, function (err, result ){
-				res.write("<title>Cart</title><link rel = 'stylesheet' type = 'text/css' href = 'style.css'>");
-				res.write("<div id = 'cart'>")
-				res.write("<table>");
-				res.write("<thead>");
-				res.write("<th style = 'width: 50%;'>Item</th>");
-				res.write("<th style = 'width: 15%;'>Quantity</th>");
-				res.write("<th style = 'width: 15%;'>Price</th>");
-				res.write("<th style = 'width: 20%;'>Total</th>");
-				res.write("</thead>");
-				res.write("<tr>");
-				res.write("<th class = 'border-bottom'></th>");
-				res.write("<th class = 'border-bottom'></th>");
-				res.write("<th class = 'border-bottom'></th>");
-				res.write("<th class = 'border-bottom'></th>");
-				res.write("</tr>");
-				var cart = result.cart;
-				for (i = 0; i < cart.length; i++)
-				{
-					var item = cart[i].cart_item;
-					var quantity = cart[i].cart_quantity;
-					var price = cart[i].cart_price;
-					var total = quantity * price;
-					res.write("<tr>");
-					res.write("<td style = 'width: 20%;'>" + item + "</td>");
-					res.write("<td style = 'width: 15%;'>" + quantity + "</td>");
-					res.write("<td style = 'width: 15%;'> $" + price + "</td>");
-					res.write("<td style = 'width: 20%;'> $" + total + "</td>");
-					res.write("</tr>")
-				}
-				res.write("</table>");
-				res.write("<form method = 'POST' action = '/checkout'>")
-				res.write("<input type = 'submit' value = 'Checkout' class = 'button'></input>")
-			    	res.write("<br>")
-			    	res.write("<br>")
-				res.write("</form>")
-				res.write("</div>")
-			});
-
-		}); 
-	})
-    })
-})
-
-app.post('/checkout', (req, res) => {
-    res.send("Your order has been processed");
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
