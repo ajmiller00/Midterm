c59c095d7818dc01bce12a7d958565f472583cdc
haijun12 <79031887+haijun12@users.noreply.github.com>
2021-12-12 11:23:08 -0600

    Update products.js

--- products.js
+++ products.js
@@ -268,9 +268,10 @@ app.post("/add", function (req, res) {
 				
 			}
 		});
-    })
 	db.close();
 	res.end();
+    })
+	
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
