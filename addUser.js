ef74a1ef315f1a8361eb730de0adf794612962df
Duru Ugurlu <70917910+duruugurlu@users.noreply.github.com>
2021-12-12 11:40:10 -0500

    login/signup

---
+++ addUser.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://amille26:cs20final@cluster0.ktqrs.mongodb.net/reveauchocolat?retryWrites=true&w=majority";
+const fs = require('fs');
+var crypto = require('crypto');
+const csv = require('csv-parser');
+
+exports.addUser = async function (pdata) {
+    MongoClient.connect(url, { useUnifiedTopology: true }, async function(err, db) {
+        if(err) { return console.log(err); return;}
+            console.log("hereee");
+          var dbo = db.db("reveauchocolat");
+          var collection = dbo.collection('users');
+
+          console.log(pdata['fname']);
+
+          await collection.find({"email" : pdata['email']}).toArray(async function(err, items) {
+            	if (err) {
+            		console.log("Error: " + err);
+            	} else {
+                    if (items.length > 0) {
+                        console.log("Email already exists");
+                        return "This email already has an account associated with it. Please try another email."
+                    } else {
+                        let salt = (Math.random() + 1).toString(36).substring(7);
+                        var password = crypto.createHash('md5').update(salt + pdata['pass']).digest('hex');
+                        console.log(password);
+                        var newData = {"fname":pdata['fname'], "lname":pdata['lname'], "email":pdata['email'], "phone":{"$numberLong":pdata['phone']},"street":pdata['street'], "city":pdata['city'], "state":pdata['state'], "zip":pdata['zip'], "password": password, "cart":[""], "salt":salt};
+
+                        await collection.insertOne(newData, function(err, res) {
+                            if(err) { console.log("Insert Error: " + err); return; }
+                            console.log("New document inserted!");
+                        });
+                    }
+            	}
+
+              console.log("Success!");
+              db.close();
+          });
+
+    });
+};
---
+++ logIn.js
@@ -0,0 +1,47 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://amille26:cs20final@cluster0.ktqrs.mongodb.net/reveauchocolat?retryWrites=true&w=majority";
+const fs = require('fs')
+var crypto = require('crypto');
+var qs = require('querystring');
+
+exports.LogInAuth = async (email, password) => {
+  var name = "";
+
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, async (err, db) => {
+      if (err) return reject(err)
+
+      const coll = await (db.db("reveauchocolat").collection('users'))
+
+      try {
+       const items = await (await coll.find({ "email": email })).toArray()
+       console.log("Items: ");
+        for (i = 0; i < items.length; i++) {
+          console.log(i + ": " + items[i].fname + " by: " + items[i].lname);
+          console.log("Print here : " + name);
+          var hash = crypto.createHash('md5').update(items[i].salt + password).digest('hex');
+          console.log(hash);
+          if (hash == items[i].password) {
+            // console.log("yaaaas we made it");
+            name = items[i].email.toString();
+          } else {
+            name = "no";
+          }
+        }
+      } catch (e) {
+        return reject(e)
+      }
+
+      console.log("Success!");
+      db.close();
+      console.log("Print " + name);
+
+      return resolve(name)
+
+    });
+  });
+};
+
+// (async () => {
+//   console.log(await this.LogInAuth('abigail.miller@tufts.edu', 'abby123'));
+// })()
